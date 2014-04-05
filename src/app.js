48b8a7969d11e953781fc3aeee551e8b36e0058d
Tom <absentisinaventulus@gmail.com>
2014-04-04 19:44:42 -0500

    no message

--- src/app.js
+++ src/app.js
@@ -3,12 +3,11 @@ var app = require('http').createServer(handler)
 , io = require('socket.io').listen(app)
 , fs = require('fs')
  
-app.listen(80);
+app.listen(8080);
  
  
 function is_mobile(req) {
   var ua = S(JSON.stringify(req.headers['user-agent']));
-  console.log(ua)
   if (ua.contains('Nexus 7') || ua.contains('Android'))
     return true;
   else return false;
@@ -16,20 +15,28 @@ function is_mobile(req) {
  
 function handler (req, res) {
   if(is_mobile(req)){
-  console.log("You're mobile!")
-  fs.readFile(__dirname + '/tablet.html', function (err, data) {
-    if (err) {
-    res.writeHead(500);
-    return res.end('Can\'t load headset html right now :/');
-    }
+    console.log("You're mobile!")
+    var extname = path.extname(filePath+"/tablet/");
+    var contentTypesByExtention = {
+      'html': 'text/html',
+      'js':   'text/javascript',
+      'css':  'text/css'
+    };
+    var contentType = contentTypesByExtention[extname] || 'text/plain';
+
+    fs.readFile(__dirname + '/tablet/index.html', function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Can\'t load headset html right now :/');
+      }
  
-    res.writeHead(200);
-    res.end(data);
+      res.writeHead(200);
+      res.end(data);
     });
   }
  
   else{
-  fs.readFile(__dirname + '/desktop.html', function (err, data) {
+  fs.readFile(__dirname + '/desktop/index.html', function (err, data) {
     if (err) {
       res.writeHead(500);
     return res.end('Error loading desktop.html! ;-;');
